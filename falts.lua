fb6e2c50769d28f019f48ca6478d1ace002fc87f
឵ <64882067+Tooxiic@users.noreply.github.com>
2024-01-30 00:35:21 +0000

    Update falts.lua

--- falts.lua
+++ falts.lua
@@ -1,20 +1,5 @@
-getgenv().config = {
-    autoFishing = true, -- true / false 
-    placetoFish = "AdvancedFishing", -- "Fishing" // "AdvancedFishing"
-    invisWater = true, -- make area's water transparent
-    
-    userToMail = "zBossPT", -- your main's username to mail all Magic Shards to
-    amountToMail = 0, -- amount to mail if set to 0 then it sends all
-    autoMail = false, -- true / false for below option
-    mailTime = "3600", -- how often to send mail (seconds)
-    
-    --collectMail = false, -- PLANNED FEATURE NOT YET
-    --collectTime = "300", -- PLANNED FEATURE NOT YET
-    
-    autoPresents = false, -- true / false (collects presents on 5 min timer)
-    
-    -- sendWebhooks = false, -- waiting for executor that supports http
-    -- webhookUrl = ""
-}
-task.wait(10)
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2ba71da818277b618eace56f899d9d3e.lua"))()
+-- v1 with Presents
+_G.Enabled = true -- true / false for auto
+_G.Area = "AdvancedFishing" -- "Fishing" / "AdvancedFishing"
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/9b9c6e316b5cc9ce3ac01b4212a49215.lua"))()
