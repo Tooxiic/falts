0f8514745d0f6722d847dbbf990931c90990b996
឵ <64882067+Tooxiic@users.noreply.github.com>
2024-01-31 00:20:56 +0000

    Update falts.lua

--- falts.lua
+++ falts.lua
@@ -1,7 +1,6 @@
 -- v1 with Presents
 _G.Enabled = true -- true / false for auto
-_G.Area = "AdvancedFishing" -- "Fishing" / "AdvancedFishing"
+_G.Area = "Fishing" -- "Fishing" / "AdvancedFishing"
 
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/9b9c6e316b5cc9ce3ac01b4212a49215.lua"))()
-task.wait(10)
 setfpscap(5)
