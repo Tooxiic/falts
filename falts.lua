c2d1c1a8974a16f2c5cddf7b18f58052ed6f717d
឵ <64882067+Tooxiic@users.noreply.github.com>
2024-02-01 18:44:38 +0000

    Update falts.lua

--- falts.lua
+++ falts.lua
@@ -4,5 +4,5 @@ _G.Enabled = true -- true / false for auto
 _G.Area = "AdvancedFishing" -- "Fishing" / "AdvancedFishing"
 
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/9b9c6e316b5cc9ce3ac01b4212a49215.lua"))()
-setfpscap(10)
+setfpscap(5)
 --game:GetService("RunService"):Set3dRenderingEnabled(false)
