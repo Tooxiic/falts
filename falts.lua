5427dd23b768b7656afdaf1c265b3b311df87a58
឵ <64882067+Tooxiic@users.noreply.github.com>
2024-02-02 14:10:31 +0000

    Update falts.lua

--- falts.lua
+++ falts.lua
@@ -1,7 +1,32 @@
 -- v1 with Presents
-_G.Enabled = true
-_G.Area = "AdvancedFishing"
+--_G.Enabled = true
+--_G.Area = "AdvancedFishing"
 
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ed804add7997f188535ed07840c724da.lua"))()
+--loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ed804add7997f188535ed07840c724da.lua"))()
+
+getgenv().config = {                 
+    autoFishing = true,              -- farm off execution or not
+    placetoFish = "AdvancedFishing", -- "Fishing" // "AdvancedFishing"
+    invisWater = true,                  -- make water transparent
+    autoPresents = true,              -- auto collect presents
+
+    autoMail = false,                  -- true // false
+    autoMailUsers = {                 
+        "Username1",                 -- Usernames to auto mail
+        "Username2",                 -- can add as many as you want
+    },
+    autoMailItem = "",                 -- "Magic Shards" // "Huge Poseidon Corgi"
+    autoMailAmount = 0,              -- amount to auto mail (for shards)
+    autoMailTimer = 300,              -- custom timer (seconds)
+
+    manualMailUsers = {
+        "Username1",                 -- Usernames to manual mail
+        "Username2",                 -- can add as many as you want
+    },
+    manualMailItem = "",             -- "Magic Shards" // "Huge Poseidon Corgi"
+    manualMailAmount = 0,              -- amount to manually mail
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/e7295942a8e53136487f943515e8fd8b.lua"))()
+
 setfpscap(5)
 --game:GetService("RunService"):Set3dRenderingEnabled(false)
