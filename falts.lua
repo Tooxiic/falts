4c29e2c16afff7f76224d9bebbf5272bea9dfb9c
឵ <64882067+Tooxiic@users.noreply.github.com>
2024-02-11 23:47:59 +0000

    Update falts.lua

--- falts.lua
+++ falts.lua
@@ -1,224 +1,143 @@
-local BigChests = {
-    [1] = "Beach",
-    [2] = "Underworld",
-    [3] = "No Path Forest",
-    [4] = "Heaven Gates"
+local Things = workspace:WaitForChild("__THINGS")
+local Active = Things.__INSTANCE_CONTAINER:WaitForChild("Active")
+local Player = game.Players.LocalPlayer
+local character = Player.Character
+local humanoidRootPart = character:FindFirstChild("HumanoidRootPart")
+local Active = Things.__INSTANCE_CONTAINER.Active
+local ActiveBlocks = Active.AdvancedDigsite.Important.ActiveBlocks
+local ActiveChests = Active.AdvancedDigsite.Important.ActiveChests
+
+local CurrentActive = function()
+    return Active:GetChildren()[1]
+end
+
+local GetChest = function()
+    for i, v in pairs(ActiveChests:GetChildren()) do
+        return v
+    end
+    return nil
+end
+ 
+local GetBlock = function()
+    local blocks = ActiveBlocks:GetChildren()
+    for i, v in pairs(ActiveBlocks:GetChildren()) do
+        local CurrentBlock = blocks[#blocks - i + 1]
+        if CurrentBlock.Color.R > 0.067 or CurrentBlock.Color.G > 0.067 or CurrentBlock.Color.B > 0.067 then
+            local coord = CurrentBlock:GetAttribute('Coord')
+            if (coord.X > 1 and coord.X < 16 and coord.Z > 1 and coord.Z < 16) then
+                return CurrentBlock
+            end
+        end
+    end
+    return nil
+end
+ 
+local Dig = function () -- yes Dig my ass out pls mmm daddy
+    local Chest = GetChest()
+    if Chest then
+        j = 1
+        while Chest.Parent == ActiveChests and j < 20 do
+            humanoidRootPart.CFrame = Chest:FindFirstChildWhichIsA("BasePart").CFrame
+            game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Instancing_FireCustomFromClient"):FireServer(CurrentActive().Name, "DigChest", Chest:GetAttribute('Coord'))
+            task.wait(0.2)
+        end
+        if j == 20 then
+            Chest.Parent = nil
+        end
+    else
+        local Block = GetBlock()
+        j = 1
+        while Block.Parent == ActiveBlocks and j < 100 do
+            humanoidRootPart.CFrame = Block.CFrame
+            game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Instancing_FireCustomFromClient"):FireServer(CurrentActive().Name, "DigBlock", Block:GetAttribute('Coord'))
+            task.wait(0.2)
+            j = j + 1
+        end
+        if Block.Parent == ActiveBlocks then
+            Block:Destroy()
+        end
+    end
+end
+
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
+local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
+
+local Window = Fluent:CreateWindow({
+    Title = "Ani's script PS99",
+    SubTitle = "by Ani",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(580, 460),
+    Acrylic = false, -- The blur may be detectable, setting this to false disables blur entirely
+    Theme = "Dark",
+    MinimizeKey = Enum.KeyCode.Delete -- Used when theres no MinimizeKeybind
+})
+
+local Tabs = {
+    Digging = Window:AddTab({ Title = "Digging", Icon = "" }),
 }
 
-repeat
-    task.wait()
-until game:IsLoaded()
+local Options = Fluent.Options
 
-local Workspace = game:GetService("Workspace")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Library = ReplicatedStorage:WaitForChild("Library")
-local Client = Library:WaitForChild("Client")
-local LocalPlayer = game:GetService("Players").LocalPlayer
-
-local zonePath
-
-local function trim(string)
-    if not string then
-        return false
-    end
-    return string:match("^%s*(.-)%s*$")
-end
-
-local function split(input, separator)
-    if separator == nil then
-        separator = "%s"
-    end
-    local parts = {}
-    for str in string.gmatch(input, "([^" .. separator .. "]+)") do
-        table.insert(parts, str)
-    end
-    return parts
-end
-
-
-local function teleportToZone(selectedZone)
-    local teleported = false
-
-    while not teleported do
-        for _, v in pairs(Workspace.Map:GetChildren()) do
-            local zoneName = trim(split(v.Name, "|")[2])
-            if zoneName and zoneName == selectedZone then
-                LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Map[v.Name].PERSISTENT.Teleport.CFrame
-                teleported = true
-                break
-            end
-        end
-        task.wait()
-    end
-end
-
-local function waitForLoad(zone)
-    for _, v in pairs(Workspace.Map:GetChildren()) do
-        local zoneName = trim(split(v.Name, "|")[2])
-        if zoneName and zoneName == zone then
-            zonePath = game:GetService("Workspace").Map[v.Name]
-            break
-        end
-    end
-
-    if not zonePath:FindFirstChild("INTERACT") then
-        local loaded = false
-        local detectLoad = zonePath.ChildAdded:Connect(function(child)
-            if child.Name == "INTERACT" then
-                loaded = true
-            end
-        end)
-
-        repeat
-            task.wait()
-        until loaded
-
-        detectLoad:Disconnect()
-    end
-
-    local function getBreakZonesAmount()
-        local counter = 0
-        for _ in pairs(zonePath.INTERACT.BREAK_ZONES:GetChildren()) do
-            counter = counter + 1
-        end
-        return counter
-    end
-
-    if getBreakZonesAmount() < 2 then
-        local loaded = false
-        local detectLoad = zonePath.INTERACT.BREAK_ZONES.ChildAdded:Connect(function(child)
-            if getBreakZonesAmount() == 2 then
-                loaded = true
-            end
-        end)
-        repeat
-            task.wait()
-        until loaded
-        detectLoad:Disconnect()
-    end
-end
-
-
-local function breakChest(zone)
-
-    local chest
-    while not chest do
-        for v in require(Client.BreakableCmds).AllByZoneAndClass(zone, "Chest") do
-            chest = v
-            break
-        end
-        task.wait()
-    end
-
-    local args = {
-        [1] = {
-
-        }
-    }
-
-    for petId, _ in pairs(require(Client.Save).Get(LocalPlayer).EquippedPets) do
-        args[1][petId] = {
-            ["targetValue"] = chest,
-            ["targetType"] = "Player"
-        }
-    end
-
-    game:GetService("ReplicatedStorage").Network.Pets_SetTargetBulk:FireServer(unpack(args))
-
-    local brokeChest = false
-    local breakableRemovedService = Workspace:WaitForChild("__THINGS").Breakables.ChildRemoved:Connect(function(breakable)
-        if breakable.Name == chest then
-            brokeChest = true
-            print("Broke chest")
-        end
-    end)
-
-    repeat
-        task.wait()
-    until brokeChest
-
-    breakableRemovedService:Disconnect()
-end
-
-
-
-require(Library.Client.PlayerPet).CalculateSpeedMultiplier = function()
-    return 200
-end
-
-local sortedKeys = {}
-for key in pairs(BigChests) do
-    table.insert(sortedKeys, key)
-end
-table.sort(sortedKeys)
-
-for _, key in ipairs(sortedKeys) do
-    local zoneName = BigChests[key]
-
-    print("Starting " .. zoneName)
-
-    teleportToZone(zoneName)
-    waitForLoad(zoneName)
-    task.wait()
-
-    task.wait(2)
-    for _, v in pairs(game:GetService("Workspace").__DEBRIS:GetChildren()) do
-
-        if v.Name == "host" then
-            local timer
-
-            pcall(function()
-                timer = v.ChestTimer.Timer.Text
-            end)
-
-            if timer ~= nil then
-                if timer == "00:00" or timer == "09:59" then
-                    print(zoneName .. " chest is available")
-                    LocalPlayer.Character.HumanoidRootPart.CFrame = zonePath.INTERACT.BREAKABLE_SPAWNS.Boss.CFrame
-                    breakChest(zoneName)
-                else
-                    print(zoneName .. " chest is not available " .. timer)
+do
+    local Toggle = Tabs.Digging:AddToggle("DigSite", {Title = "Auto AdvancedDigsite", Default = false})
+    Toggle:OnChanged(function()
+        Toggle = Options.DigSite.Value
+        game:GetService("RunService").Stepped:Connect(function()
+            if Toggle then
+                for i,v in next, Player.Character:GetChildren() do
+                    if v:IsA("BasePart") then
+                        v.CanCollide = false
+                        v.Velocity = Vector3.new(0,0,0)
+                    end
                 end
-                v:Destroy()
-                break
+            else
+                for i,v in next, Player.Character:GetChildren() do
+                    if v:IsA("BasePart") then
+                        v.CanCollide = true
+                    end
+                end
             end
-        end
-    end
-    warn("Finished " .. zoneName)
+        end)
+        spawn(function()
+            while Toggle and wait() do
+                pcall(function()
+                    Dig()
+                end)
+            end
+        end)
+        spawn(function()
+            while Toggle and wait() do
+                pcall(function()
+                    for i, v in workspace.__THINGS.Orbs:GetChildren() do
+                        v.Position = humanoidRootPart.Position
+                    end
+                    task.wait(1)
+                end)
+            end
+        end)
+    end)
 end
+SaveManager:SetLibrary(Fluent)
+InterfaceManager:SetLibrary(Fluent)
+SaveManager:IgnoreThemeSettings()
+SaveManager:SetIgnoreIndexes({})
+InterfaceManager:SetFolder("AniHub")
+SaveManager:SetFolder("AniHub/specific-game")
 
-print("Server hopping")
+InterfaceManager:BuildInterfaceSection(Tabs.Digging)
+SaveManager:BuildConfigSection(Tabs.Digging)
 
-task.wait()
 
-local sfUrl = "https://games.roblox.com/v1/games/%s/servers/Public?sortOrder=%s&limit=%s&excludeFullGames=true" 
-local req = request({ Url = string.format(sfUrl, 8737899170, "Desc", 50) }) 
-local body = game:GetService("HttpService"):JSONDecode(req.Body) 
-local deep = math.random(1, 3)
-if deep > 1 then
-    for i = 1, deep, 1 do
-        req = request({ Url = string.format(sfUrl .. "&cursor=" .. body.nextPageCursor, 8737899170, "Desc", 50) }) 
-        body = game:GetService("HttpService"):JSONDecode(req.Body) 
-        task.wait(0.1)
-    end
-end
-local servers = {} 
-if body and body.data then
-    for i, v in next, body.data do
-        if type(v) == "table" and tonumber(v.playing) and tonumber(v.maxPlayers) and v.playing < v.maxPlayers and v.id ~= game.JobId then
-            table.insert(servers, 1, v.id)
-        end
-    end
-end
-local randomCount = #servers
-if not randomCount then
-    randomCount = 2
-end
-while true do
-    game:GetService("TeleportService"):TeleportToPlaceInstance(8737899170, servers[math.random(1, randomCount)], LocalPlayer)
-    task.wait(0.5)
-    game:GetService("TeleportService"):Teleport(8737899170, LocalPlayer)
-end
+Window:SelectTab(1)
+
+Fluent:Notify({
+    Title = "Ani's fix script'",
+    Content = "script loaded.",
+    Duration = 4
+})
+
+SaveManager:LoadAutoloadConfig()
 
 --setfpscap(3)
 --game:GetService("RunService"):Set3dRenderingEnabled(false)
