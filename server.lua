4c1054783ea3c66b3308392f2b472970d76129e1
឵ <64882067+Tooxiic@users.noreply.github.com>
2024-02-19 16:59:19 +0000

    Create server.lua

---
+++ server.lua
@@ -0,0 +1,37 @@
+local SERVER_URL = "YOUR_SERVER_URL"
+
+local data = request(
+    {
+        Url = SERVER_URL,
+        Method = "GET"
+    }
+)
+
+local heartbeatDelay = game.HttpService:JSONDecode(data.Body)["heartbeatDelay"]
+
+local function sendRequest(reqType)
+    return request(
+        {
+            Url = SERVER_URL,
+            Method = "POST",
+            Body = {
+                requestType = reqType,
+                account = game:GetService("Players").LocalPlayer.Name
+            }
+        }
+    )
+end
+
+sendRequest("START")
+
+game:GetService("NetworkClient").ChildRemoved:Connect(function()
+    while true do
+        sendRequest("DISCONNECTED")
+        task.wait(1)
+    end
+end)
+
+while true do
+    sendRequest("HEARTBEAT")
+    task.wait(heartbeatDelay)
+end
