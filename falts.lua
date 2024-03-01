af220c62bfa9f19cc2dfbfaa33eeacfad8a41025
឵ <64882067+Tooxiic@users.noreply.github.com>
2024-03-01 03:47:03 +0000

    Update falts.lua

--- falts.lua
+++ falts.lua
@@ -2,12 +2,12 @@ getgenv().AuthKey = "HUGE_jBFvQnaZ3P5s"
 getgenv().LoadSettings = {
     Example_Setting = Example_Value
 }
---loadstring(game:HttpGet("https://hugegames.io/ps99"))()
+loadstring(game:HttpGet("https://hugegames.io/ps99"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Tooxiic/falts/main/autorejoiner.lua"))()
 getgenv().autoChest = true
 
 getgenv().autoChestConfig = {
-    START_DELAY = 2, -- delay before starting
+    START_DELAY = 5, -- delay before starting
     SEND_PETS = true, -- send pets to break chests (if false, it shouldn't interfere with autofarm)
     SERVER_HOP = true, -- server hop after breaking all chests
     SERVER_HOP_DELAY = 1, -- delay in seconds before server hopping (set to 0 for no delay)
